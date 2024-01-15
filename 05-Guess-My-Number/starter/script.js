05f1df1478f1362ae0cdd2d6cc3a0a6bd8a4f523
yadavXraju <yadavxraju@gmail.com>
2024-01-16 01:30:47 +0530

    commit fifth

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,16 +1,22 @@
 'use strict';
 
 
-let value = 10;
+
 
 //  document.querySelector('.check').addEventListener('click',function(){
 //     const doc =  document.querySelector('.guess').value;
 //     console.log(doc)
 //  })
+let highScore = 0;
 
 const number = Math.trunc(Math.random()*20)+1;
 document.querySelector('.number').textContent = number
+
+
 console.log(number)
+
+let score = Number(document.querySelector('.score').textContent);
+
 
 
 document.querySelector('.check').addEventListener('click',function(){
@@ -20,7 +26,39 @@ document.querySelector('.check').addEventListener('click',function(){
     if(!guess){
         document.querySelector('.message').textContent = '⛔ No Number!';
 
+    }else if(guess===number){
+         document.querySelector('.message').textContent = '🎉Correct Guess';
+         document.querySelector('body').style.backgroundColor = '#60b347'
+         document.querySelector('.number').style.width = '30rem'
+         document.querySelector('.number').textContent = number
+         if(score>highScore){
+            highScore = score;
+            document.querySelector('.highscore').textContent = highScore
+         }
+    }else if(guess<number){
+        if(score>1){
+            document.querySelector('.message').textContent = '〽👀 Too Low'
+            score--;
+
+        }
+        document.querySelector('.score').textContent = score;
+    }else if(guess>number){
+        if(score>1){
+
+            document.querySelector('.message').textContent = '⚡Too high'
+            score--;
+            document.querySelector('.score').textContent = score;
+        }
     }
+
+
+    document.querySelector('.again').addEventListener('click',function(){
+       document.querySelector('.guess').value = '';
+       document.querySelector('body').style.backgroundColor ='#222';
+       document.querySelector('.message').textContent = 'Start guessing...';
+       document.querySelector('.score').textContent = 20;
+       document.querySelector('.number').style.width = '15rem'
+    })
     
 })
 
